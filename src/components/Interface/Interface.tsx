7001e4d13b756ab43e4d11c32fdea3bd526e64f9
evgen <evgst.gl@gmail.com>
2023-07-15 23:15:46 +0700

    Range input

--- src/components/Interface/Interface.tsx
+++ src/components/Interface/Interface.tsx
@@ -1,15 +1,27 @@
 import React, {FC} from "react";
-import Button from "../Button/Button";
 import styles from './Interface.module.scss';
+import Button from "../Button/Button";
+import RangeInput from "../RangeInput/RangeInput";
 interface Props {
     children?: React.ReactNode
 }
 const GameInterface: FC<Props> = () => {
     return (
         <div className={styles['game-interface']}>
-            <Button title={'Make random alive cells'}>Create</Button>
-            <Button title={'Start alive cells evolution'}>Evolve</Button>
-            <Button title={'Kill all alive cells'}>Reset</Button>
+            <div className={styles.giRow}>
+                <div className={styles.giCol}>
+                    <Button title={'Make random alive cells'}>Create</Button>
+                </div>
+                <div className={styles.giCol}>
+                    <Button title={'Start alive cells evolution'}>Evolve</Button>
+                </div>
+                <div className={styles.giCol}>
+                    <Button title={'Kill all alive cells'}>Reset</Button>
+                </div>
+                <div className={styles.giCol}>
+                    <RangeInput id={'speedControl'} min={100} max={500} value={'300'} step={1} />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/RangeInput/RangeInput.tsx
@@ -0,0 +1,30 @@
+import {FC} from "react";
+import styles from './RangeInput.module.scss';
+interface Props {
+    id?: string,
+    min: number,
+    max: number,
+    value: string,
+    step: number,
+    name?: string,
+    title?: string,
+    disabled?: boolean
+}
+const RangeInput: FC<Props> = ({ id, name, min, max, value, step, disabled, title}) => {
+    return (
+        <input
+            className={styles['range-input']}
+            type="range"
+            id={id}
+            name={name}
+            min={min}
+            max={max}
+            value={value}
+            step={step}
+            disabled={disabled}
+            title={title}
+        />
+    )
+}
+
+export default RangeInput;
